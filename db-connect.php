b85c3410ddb0e302ef581c9d05cb1745350732f6
mashudu luvhengo <mluvhengo25@gmail.com>
2022-05-26 23:38:08 +0200

    connected register page to db

---
+++ db-connect.php
@@ -0,0 +1,20 @@
+<?php 
+
+$server = "localhost";
+$username = "root";
+$pswd = "";
+$dbName = "lotus_thrift_bookstore";
+
+$dbconnect = mysqli_connect($server, $username, $pswd, $dbName);
+
+if($dbconnect === false){
+    echo "Connection error: " . mysqli_connect_error($dbconnect);
+}
+else{
+    echo " Connection successful";
+}
+
+// mysqli_close($dbconnect);
+// $dbconnect = false;
+
+?>
